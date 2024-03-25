1a1545d5ff8da78910acdfd798dbb08de3085eb1
vvasiutynska <vvasiutynska@gmail.com>
2024-03-25 18:46:08 +0200

    js_homework6 added

---
+++ Arrays/script.js
@@ -0,0 +1,79 @@
+function myBlend(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+}
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(arr);
+
+myBlend(arr);
+console.log(arr);
+
+
+const company = {
+    name: 'Велика Компанія',
+    type:'Головна компанія',
+    platform: 'Платформа для продажу квитків',
+    sellsSolution: 'Рішення для продажу квитків',
+    clients: [
+        {
+            name: 'Клієнт 1',
+            type: 'subCompany',
+            uses: 'ПО для продажу квитків',
+            sells: 'Рішення для продажу квитків',
+            partners: [
+                {
+                    name: 'Клієнт 1.1',
+                    type: 'subSubCompany',
+                    uses: 'Рішення для продажу квитків',
+                    sells: 'Рішення для продажу квитків',
+                },
+                {
+                    name: 'Клієнт 1.2',
+                    type: 'subSubCompany',
+                    uses: 'Рішення для продажу квитків',
+                    sells: 'Рішення для продажу квитків',
+                    partners: [
+                        {
+                            name: 'Клієнт 1.2.3',
+                            type: 'subSubCompany',
+                            uses: 'Рішення для продажу квитків',
+                            sells: 'Рішення для продажу квитків',
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            name: 'Клієнт 2',
+            type: 'subCompany',
+            uses: 'ПО для продажу квитків',
+            sells: 'Рішення для продажу квитків'
+        }
+    ]
+};
+
+function findValueByKey(companyName, company) {
+    if (company.name === companyName) {
+        const companyCopy = { ...company };
+        delete companyCopy.clients;
+        return companyCopy;
+    } else if (company.clients) {
+        for (let client of company.clients) {
+            const result = findValueByKey(companyName, client);
+            if (result) {
+                return result;
+            }
+        }
+    }
+    return null;
+}
+
+const companyName = 'Велика Компанія';
+const companyInfo = findValueByKey(companyName, company);
+console.log(companyInfo);
+
+
+
